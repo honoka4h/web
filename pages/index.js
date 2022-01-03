502fb7978706512bc098b092aa65bf0da7904de9
honoka4h <honoka4h@gmail.com>
2022-01-03 17:10:14 +0900

    김도현 update v0.2

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Index () {
   return(
     <div>
       <div className="gimdohyun">김도현은 씹병신이다 ㄹㅇ크크루핑크봉구스밥버거 -김도현-</div>
-     <div className="hong"><iframe width="560" height="315" src="https://www.youtube.com/embed/dd7N_Bz-nto" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+      <div className="hong">홍준표 지지안하면 문재인</div>
      김X현 전용 로그인
      E-mail: 
       <input />
@@ -18,6 +18,7 @@ function Index () {
 
       .hong {
         text-align: center;
+        color: blue;
       }
 
       `}</style>

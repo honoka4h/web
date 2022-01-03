595d1d65a9af91107ff5179358488732f6206548
honoka4h <honoka4h@gmail.com>
2022-01-03 17:02:17 +0900

    김도현 사이트 update v0.1

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,27 @@
 function Index () {
   return(
-    <div>김도현은 씹병신이다 ㄹㅇ크크루핑크봉구스밥버거 -김도현-</div>
+    <div>
+      <div className="gimdohyun">김도현은 씹병신이다 ㄹㅇ크크루핑크봉구스밥버거 -김도현-</div>
+     <div className="hong"><iframe width="560" height="315" src="https://www.youtube.com/embed/dd7N_Bz-nto" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+     김X현 전용 로그인
+     E-mail: 
+      <input />
+      Password:
+      <input />
+      <button>로그인</button>
+      <style jsx>{`
+      .gimdohyun {
+        font-size: 64px;
+        text-align: center;
+        color: red;
+      }
+
+      .hong {
+        text-align: center;
+      }
+
+      `}</style>
+    </div>
   )
 }
 
